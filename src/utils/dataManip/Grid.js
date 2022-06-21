455ab493b308caba279402c024378a6ee6736bdc
Justin <thejasumasa@gmail.com>
2022-06-21 14:46:16 +0900

    set up test environment and update webpack config.js in the main code part2

--- src/utils/dataManip/Grid.js
+++ src/utils/dataManip/Grid.js
@@ -1,47 +1,38 @@
 class Graph {
-    constructor(pattern) {
-      this.pattern = pattern;
-      this.matrix = makeAdjMatrix(this.pattern);
-    }
-  
-    makeAdjMatrix() {
-      const vertices = this.pattern()
-      vertices.forEach((i) => {
-        const xArray = [];
-        vertices.forEach((j) => {
-          if (
-            edges.some(
-              (edge) =>
-                (edge[0] === i && edge[1] === j) ||
-                (edge[0] === j && edge[1] === i)
-            )
-          ) {
-            xArray.push(1);
-          } else {
-            xArray.push(0);
-          }
-        });
-        console.log(xArray);
-        yArray.push(xArray);
+  constructor(pattern) {
+    this.pattern = pattern;
+    this.nodes = Object.keys(pattern);
+    this.matrix = this.makeAdjMatrix(this.pattern);
+  }
+
+  makeAdjMatrix(pattern) {
+    const yArray = [];
+    this.nodes.forEach((i) => {
+      const xArray = [];
+      this.nodes.forEach((j) => {
+        pattern[j].includes(i) ? xArray.push(1) : xArray.push(0);
       });
-      return yArray;
-    }
-  
-    setAdjArray(edge1, edge2, vertices, adjArray) {
-      adjArray[vertices.indexOf(edge1)][vertices.indexOf(edge2)] = 1;
-      adjArray[vertices.indexOf(edge2)][vertices.indexOf(edge1)] = 1;
-    }
-  
-    checkAdj(edge1, edge2, vertices, adjArray) {
-      if (adjArray[vertices.indexOf(edge1)][vertices.indexOf(edge2)] === 1) {
-        return true;
-      } else if (
-        adjArray[vertices.indexOf(edge2)][vertices.indexOf(edge1)] === 1
-      ) {
-        return true;
-      } else {
-        return false;
-      }
+      yArray.push(xArray);
+    });
+    return yArray;
+  }
+
+  setAdjArray(edge1, edge2, vertices, adjArray) {
+    adjArray[vertices.indexOf(edge1)][vertices.indexOf(edge2)] = 1;
+    adjArray[vertices.indexOf(edge2)][vertices.indexOf(edge1)] = 1;
+  }
+
+  checkAdj(edge1, edge2) {
+    if (
+      this.matrix[this.nodes.indexOf(edge1)][this.nodes.indexOf(edge2)] === 1
+    ) {
+      return true;
+    } else if (
+      this.matrix[this.nodes.indexOf(edge2)][this.nodes.indexOf(edge1)] === 1
+    ) {
+      return true;
+    } else {
+      return false;
     }
   }
-  
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-loaders: [
-  { test: /\.json$/, loader: "json" },
-  // other loaders
-];
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [{ test: /\.json$/, loader: "json" }],
+  },
+};
