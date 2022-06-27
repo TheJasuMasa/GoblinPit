f72dea12690b893159452e0e392109975e49cb77
Jasumasa <thejasumasa@gmail.com>
2022-06-27 22:34:21 +0900

    add card dependency outline to card class

--- testenv/src/generators/Cards.js
+++ testenv/src/generators/Cards.js
@@ -1,5 +1,28 @@
 import * as cards from "./cardDefs";
 
+/*
+
+B=====D~~~~ BIG ASS TO-DO ~~~~Cl=====l3
+
+incorporate limbs into combat
+
+create combat loop
+
+create functions for taking in user stats vs opponent stats (factoring in armor mods) to check for hit chance/effectiveness
+
+give cards a method to calculate the possible ranges of their effective areas
+
+account for all possible targets/target ranges for cards (i.e. self/target/self-aoe/target-aoe)
+
+dismemberment system (see below)
+
+system for managing card access (i.e. if a limb depends on a card, disabling the card)
+
+
+
+*/
+
+
 export class Cards {
   constructor(defs) {
     //Should be an array
