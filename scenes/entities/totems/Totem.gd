6156516b308d19fcf45782b6d318452c7dfcb903
thejasumasa@gmail.com <thejasumasa@gmail.com>
2023-10-02 13:56:37 +0900

    add basic mouse hover functionality over totems, a visual element to the totem bar for debug purposes and an additional couple of files to the admin folder

--- scenes/entities/totems/Totem.gd
+++ scenes/entities/totems/Totem.gd
@@ -14,3 +14,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_mouse_entered():
+	Input.mouse_mode(2)
+	print("Entered")
+	print(global_position)
+	var currentPosition = global_position
+	global_position.y = currentPosition.y - 10
+	print(global_position)
+
+
+func _on_area_2d_mouse_exited():
+	print("Exited")
+	print(global_position)
+	var currentPosition = global_position
+	global_position.y = currentPosition.y + 10
+	print(global_position)
